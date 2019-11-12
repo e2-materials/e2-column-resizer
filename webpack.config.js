f5e2e7f0c53870350078ca18cd2800caf56e8ef1
ningbo.kang <ningbo.kang@ecarx.com.cn>
2019-11-12 18:40:47 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: "./src/ColumnResizer.js",
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: [
+                    path.resolve(__dirname, "node_modules")
+                ],
+                use: [{loader: "babel-loader"}]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".css"]
+    },
+    devtool: "source-map",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/",
+        filename: "column-resizer.js",
+        libraryTarget: "umd",
+        library: "ColumnResizer",
+        globalObject: "this"
+    },
+    optimization: {
+        nodeEnv: 'production',
+        minimizer: [
+            new UglifyJSPlugin({
+                sourceMap: true,
+                uglifyOptions: {
+                    ecma: 6,
+                    mangle: {
+                        keep_fnames: true
+                    },
+                    output: {
+                        comments: false
+                    }
+                }
+            })
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ]
+};
